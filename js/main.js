64e6bbab6d4864bd0e923706acbc6882f05f56c8
LuisCastilloDaga <cas20044@byui.edu>
2022-04-23 15:33:39 -0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function lastUpdate() {
+    const d = new Date(document.lastModified);
+    const day = d.getDate();
+    const month = d.getMonth();
+    const year = d.getFullYear();
+    const hours = d.getHours();
+    const minutes = d.getMinutes();
+    const seconds = d.getSeconds();
+
+    const fulldate = `Last Updated: ${month}/${day}/${year} ${hours}:${minutes}:${seconds}`;
+
+
+    document.getElementById('lastmodification').textContent = fulldate;
+
+    document.getElementById('year').textContent = new Date().getFullYear();
+
+}
