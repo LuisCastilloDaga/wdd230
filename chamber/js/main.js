ca7b8aa4ddcf5c699291cd0f2edd81da776d82a5
LuisCastilloDaga <cas20044@byui.edu>
2022-05-21 19:27:44 -0500

    initial commit

--- chamber/js/main.js
+++ chamber/js/main.js
@@ -16,17 +16,33 @@ document.getElementById('lastmodification').textContent = fulldate;
 document.querySelector('#year').textContent = new Date().getFullYear();
 
 document.querySelector('.date-large').textContent = Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
-	now
+    now
 );
 document.querySelector('.date-mobile').textContent = Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
-	now
+    now
 );
 
+/*::::Mobile Menu:::::*/
 
 const hamb = document.querySelector('#hamburguer');
 const menu = document.querySelector('#menu');
 
-hamb.onclick = function(){
+hamb.onclick = function () {
     menu.classList.toggle('show-menu');
     hamb.classList.toggle('xChange');
 }
+
+
+/*:::::Top banner::::: */
+
+const bannerContent = document.querySelector('.banner');
+
+if ((now.getDay() != 1) && (now.getDay() != 2)) {
+    bannerContent.style.display = "none";
+} else {
+    bannerContent.style.display = "block";
+}
+
+
+/*::::::Weather widget:::: */
+/* !function (d, s, id) { var js, fjs = d.getElementsByTagName(s)[0]; if (!d.getElementById(id)) { js = d.createElement(s); js.id = id; js.src = 'https://weatherwidget.io/js/widget.min.js'; fjs.parentNode.insertBefore(js, fjs); } }(document, 'script', 'weatherwidget-io-js'); */
