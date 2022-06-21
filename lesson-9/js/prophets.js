594f679ff6a3b617b59ea993903ef63e33d2d426
LuisCastilloDaga <cas20044@byui.edu>
2022-06-21 13:21:36 -0500

    initial commit

--- lesson-9/js/prophets.js
+++ lesson-9/js/prophets.js
@@ -11,24 +11,35 @@ fetch(requestURL)
     prophets.forEach(displayProphets);
   });
 
-  function displayProphets(prophet) {
+function afterOrder(order) {
+  return (order != 1 && order != 2) ? 'th' : (order == 1) ? 'st' : 'nd';
+}
+
+function displayProphets(prophet) {
     // Create elements to add to the document
     let card = document.createElement('section');
     let h2 = document.createElement('h2');
+    let p = document.createElement('p');
+    let p2 = document.createElement('p');
     let portrait = document.createElement('img');
   
     // Change the textContent property of the h2 element to contain the prophet's full name
-    h2.textContent = prophet.name + ' ' + prophet.lastname;
+    h2.textContent = `${prophet.name} ${prophet.lastname}`;
+    p.textContent = `Date of Birth: ${prophet.birthdate}`;
+    p2.textContent = `Place of Birth: ${prophet.birthplace}`;
   
     // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
     portrait.setAttribute('src', prophet.imageurl);
-    portrait.setAttribute('alt', 'Portait of ' + prophet.name + ' ' + prophet.lastname);
+    portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname} - ${prophet.order}${afterOrder(prophet.order)} Latter-day President`);
     card.setAttribute('loading', 'lazy');
   
     // Add/append the section(card) with the h2 element
     card.appendChild(h2);
+    card.appendChild(p);
+    card.appendChild(p2)
     card.appendChild(portrait);
+
   
     // Add/append the existing HTML div with the cards class with the section(card)
     document.querySelector('div.cards').appendChild(card);
-  }
+}
