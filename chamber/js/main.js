79e2fe9daecb2a6bc7106c65bc59de8bfd1a40d3
LuisCastilloDaga <cas20044@byui.edu>
2022-06-23 15:34:55 -0500

    initial commit

--- chamber/js/main.js
+++ chamber/js/main.js
@@ -62,7 +62,7 @@ if (totalTime!=null) {
     } else if (calculateTime == 1){
         totalTime.textContent = `Your last visit was ${calculateTime} day ago.`;
     }else{
-        totalTime.textContent = `Welcome. This is your fisrt day with us!`;
+        totalTime.textContent = `Welcome. Your visit was less than a day ago.`;
     }
 
 
