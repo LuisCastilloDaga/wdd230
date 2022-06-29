1973bff1388f3719d043bb3508c1c4297cf01fbb
LuisCastilloDaga <cas20044@byui.edu>
2022-06-29 15:39:48 -0500

    initial commit

--- lesson-10/js/weatherapi.js
+++ lesson-10/js/weatherapi.js
@@ -3,4 +3,27 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url = '{insert the correct URL here}';
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&appid=6725ba7850ae02903340395b04dba12f&units=imperial';
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // this is for testing the call
+        
+        displayResults(data);
+
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+  apiFetch();
+
+function displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+  }
