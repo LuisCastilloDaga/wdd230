2fbf7d106ae131efe12be6321d1421a0c8a7f98e
LuisCastilloDaga <cas20044@byui.edu>
2022-06-29 16:15:09 -0500

    initial commit

--- lesson-10/js/weatherapi.js
+++ lesson-10/js/weatherapi.js
@@ -26,4 +26,11 @@ async function apiFetch() {
 
 function displayResults(weatherData) {
     currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
-  }
+    
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+}
